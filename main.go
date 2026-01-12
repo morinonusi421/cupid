7adb9c8c1651887ffb528f68332c8268196fe329
morinonusi421 <morinonusi421@icloud.com>
2026-01-12 20:32:25 +0900

    add: おうむがえしbotを作成

--- main.go
+++ main.go
@@ -2,14 +2,88 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+	"github.com/line/line-bot-sdk-go/v8/linebot/webhook"
 )
 
 func main() {
+	// .envファイルを読み込む
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+	channelToken := os.Getenv("LINE_CHANNEL_TOKEN")
+
+	if channelSecret == "" || channelToken == "" {
+		log.Fatal("LINE_CHANNEL_SECRET and LINE_CHANNEL_TOKEN must be set")
+	}
+
+	// LINE Messaging APIクライアントを作成
+	bot, err := messaging_api.NewMessagingApiAPI(channelToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ヘルスチェック用エンドポイント
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!!!!!!!!!!")
+		fmt.Fprintf(w, "Cupid LINE Bot is running")
 	})
 
-	fmt.Println("Server starting on :8080")
-	http.ListenAndServe(":8080", nil)
+	// LINE Webhook エンドポイント
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		// Webhookイベントをパース
+		callbackRequest, err := webhook.ParseRequest(channelSecret, r)
+		if err != nil {
+			log.Println("Failed to parse request:", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// 各イベントを処理
+		for _, event := range callbackRequest.Events {
+			switch e := event.(type) {
+			case webhook.MessageEvent:
+				// テキストメッセージの場合
+				switch message := e.Message.(type) {
+				case webhook.TextMessageContent:
+					// オウム返し
+					replyText := message.Text
+
+					_, err = bot.ReplyMessage(
+						&messaging_api.ReplyMessageRequest{
+							ReplyToken: e.ReplyToken,
+							Messages: []messaging_api.MessageInterface{
+								messaging_api.TextMessage{
+									Text: replyText,
+								},
+							},
+						},
+					)
+					if err != nil {
+						log.Println("Failed to reply message:", err)
+					} else {
+						log.Printf("Replied: %s", replyText)
+					}
+				}
+			}
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on :%s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
