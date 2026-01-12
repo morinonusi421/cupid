6ed3f582401962a87826d6ed5476a35e80562073
morinonusi421 <morinonusi421@icloud.com>
2026-01-12 19:19:01 +0900

    chore: test commit(make deploy検証用)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World!!!!!!!!!!")
 	})
 
 	fmt.Println("Server starting on :8080")
