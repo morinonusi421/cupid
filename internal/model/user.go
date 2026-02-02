668cfd32c539d01ffc0ef0d2a135aa973fa9d6df
morinonusi421 <morinonusi421@icloud.com>
2026-02-02 21:47:59 +0900

    docs: todoを追加

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,11 +1,15 @@
 package model
 
 // User はユーザーのドメインモデル
+// TODO: Model層の充実化 - Validationメソッドとbusiness logicを追加
+//   - IsValidName(), IsValidBirthday() などのValidation
+//   - CanCompleteRegistration(), CompleteRegistration() などの状態遷移
+//   - ドメインロジックをServiceから移動してModelに集約
 type User struct {
 	LineID           string
 	Name             string
 	Birthday         string
-	RegistrationStep int    // 0: 未登録, 1: 登録完了
+	RegistrationStep int // 0: 未登録, 1: 登録完了
 	RegisteredAt     string
 	UpdatedAt        string
 }
