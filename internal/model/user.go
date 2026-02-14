2d2d8da3ecf96520c0020006027ca9c25c41b67c
morinonusi421 <morinonusi421@icloud.com>
2026-02-14 21:40:34 +0900

    docs: 古いtodoを削除

--- internal/model/user.go
+++ internal/model/user.go
@@ -7,10 +7,6 @@ import (
 )
 
 // User はユーザーのドメインモデル
-// TODO: Model層の充実化 - Validationメソッドとbusiness logicを追加
-//   - IsValidName(), IsValidBirthday() などのValidation
-//   - CanCompleteRegistration(), CompleteRegistration() などの状態遷移
-//   - ドメインロジックをServiceから移動してModelに集約
 type User struct {
 	LineID             string
 	Name               string
